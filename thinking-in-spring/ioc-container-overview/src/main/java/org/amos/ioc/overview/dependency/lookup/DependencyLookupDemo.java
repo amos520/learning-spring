33abe0670b4f3a4013a7f177137ad5ad586a56eb
amos <aoms520@foxmail.com>
2020-03-07 23:44:09 +0800

    依赖查找： 名称 类型 实时 延迟 单个 集合

--- thinking-in-spring/ioc-container-overview/src/main/java/org/amos/ioc/overview/dependency/lookup/DependencyLookupDemo.java
+++ thinking-in-spring/ioc-container-overview/src/main/java/org/amos/ioc/overview/dependency/lookup/DependencyLookupDemo.java
@@ -1,11 +1,17 @@
 package org.amos.ioc.overview.dependency.lookup;
 
+import org.amos.ioc.overview.annotation.Super;
 import org.amos.ioc.overview.domain.User;
 import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.ListableBeanFactory;
+import org.springframework.beans.factory.ObjectFactory;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.Map;
 
 /**
  * 依赖查找示例
+ * 通过名称来查找
  * Date: 2020/3/7 18:12
  *
  * @author: amos
@@ -15,7 +21,45 @@ public class DependencyLookupDemo {
 
     public static void main(String[] args) {
         BeanFactory beanFactory = new ClassPathXmlApplicationContext("classpath:/META-INF/dependency-lookup-context.xml");
+//        lookupInRealTime(beanFactory);
+//        looupInLazy(beanFactory);
+        //按照类型查找
+        lookupByType(beanFactory);
+        //按照类型查找集合
+        lookupCollectionByType(beanFactory);
+        //通过注解查找对象
+        lookupByAnnotationType(beanFactory);
+    }
+
+    private static void lookupByAnnotationType(BeanFactory beanFactory) {
+        if(beanFactory instanceof ListableBeanFactory){
+            ListableBeanFactory listableBeanFactory = (ListableBeanFactory) beanFactory;
+            Map<String, User> users = (Map) listableBeanFactory.getBeansWithAnnotation(Super.class);
+            System.out.println("查找所有@Super："+users);
+        }
+    }
+
+    private static void lookupCollectionByType(BeanFactory beanFactory) {
+        if(beanFactory instanceof ListableBeanFactory){
+            ListableBeanFactory listableBeanFactory = (ListableBeanFactory) beanFactory;
+            Map<String, User> users = listableBeanFactory.getBeansOfType(User.class);
+            System.out.println("查找所有："+users);
+        }
+    }
+
+    private static void lookupByType(BeanFactory beanFactory) {
+        User user = beanFactory.getBean(User.class);
+        System.out.println("实时查找："+user);
+    }
+
+    private static void looupInLazy(BeanFactory beanFactory) {
+        ObjectFactory<User> objectFactory = (ObjectFactory<User>) beanFactory.getBean("objectFactory");
+        User user = objectFactory.getObject();
+        System.out.println("间接查找："+user);
+    }
+
+    private static void lookupInRealTime(BeanFactory beanFactory) {
         User user = (User) beanFactory.getBean("user");
-        System.out.println(user);
+        System.out.println("实时查找："+user);
     }
 }
--- thinking-in-spring/ioc-container-overview/src/main/java/org/amos/ioc/overview/domain/User.java
+++ thinking-in-spring/ioc-container-overview/src/main/java/org/amos/ioc/overview/domain/User.java
@@ -20,6 +20,14 @@ public class User {
         this.id = id;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     @Override
     public String toString() {
         return "User{" +
