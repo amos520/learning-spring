e079ccd1762c04b657a45e3726ed84b4cf7595b5
amos <aoms520@foxmail.com>
2020-03-19 22:49:32 +0800

    第三章 （23-25）

--- thinking-in-spring/ioc-container-overview/src/main/java/org/amos/ioc/overview/dependency/injection/DependencyInjectionDemo.java
+++ thinking-in-spring/ioc-container-overview/src/main/java/org/amos/ioc/overview/dependency/injection/DependencyInjectionDemo.java
@@ -3,6 +3,7 @@ package org.amos.ioc.overview.dependency.injection;
 import org.amos.ioc.overview.repository.UserRepository;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.env.Environment;
 
 /**
  * 依赖查注入示例
@@ -18,9 +19,19 @@ public class DependencyInjectionDemo {
         //配置xml 配置文件
         //启动 Spring 应用上下文
         BeanFactory beanFactory = new ClassPathXmlApplicationContext("classpath:/META-INF/dependency-injection-context.xml");
+        //自定义 Bean
         UserRepository userRepository = beanFactory.getBean("userRepository", UserRepository.class);
         System.out.println(userRepository.getUsers());
 
+
+        //依赖注入-内建依赖
+        System.out.println(userRepository.getBeanFactory());
+
+        //依赖查找(错误)
+//        System.out.println(beanFactory.getBean(BeanFactory.class));
+
+        //容器内建 Bean 容器初始化内部的Bean
+        Environment environment = beanFactory.getBean(Environment.class);
     }
 
 }
--- thinking-in-spring/ioc-container-overview/src/main/java/org/amos/ioc/overview/repository/UserRepository.java
+++ thinking-in-spring/ioc-container-overview/src/main/java/org/amos/ioc/overview/repository/UserRepository.java
@@ -1,11 +1,16 @@
 package org.amos.ioc.overview.repository;
 
 import org.amos.ioc.overview.domain.User;
+import org.springframework.beans.factory.BeanFactory;
 
 import java.util.Collection;
 
 public class UserRepository {
-    private Collection<User> users;
+    private Collection<User> users;//自定义
+
+
+
+    private BeanFactory beanFactory;//内建非 Bean对象（依赖）
 
     public Collection<User> getUsers() {
         return users;
@@ -13,5 +18,13 @@ public class UserRepository {
 
     public void setUsers(Collection<User> users) {
         this.users = users;
+    }
+
+    public BeanFactory getBeanFactory() {
+        return beanFactory;
+    }
+
+    public void setBeanFactory(BeanFactory beanFactory) {
+        this.beanFactory = beanFactory;
     }
 }
